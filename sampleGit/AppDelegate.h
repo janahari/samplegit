90dd9c277c2bca2bcb981dfe0777a6b5f3f091ab
Subhash <subhash@subhash.local>
2016-04-28 10:43:22 +0530

    Initial Commit

---
+++ sampleGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sampleGit
+//
+//  Created by Subhash on 4/28/16.
+//  Copyright © 2016 Happiestminds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sampleGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sampleGit
+//
+//  Created by Subhash on 4/28/16.
+//  Copyright © 2016 Happiestminds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
